2dc03170562f992a41e10b2187ad2acdfe3ce93a
darek <darek@darekzon.com>
2011-07-03 21:53:56 +0200

    add Senmail binding change ProductRepository* to ProductsListRepository*

--- src/com/darekzon/shoppinglist/modules/RepositoryModule.java
+++ src/com/darekzon/shoppinglist/modules/RepositoryModule.java
@@ -2,8 +2,10 @@ package com.darekzon.shoppinglist.modules;
 
 import com.darekzon.shoppinglist.mongoose.EntityManager;
 import com.darekzon.shoppinglist.mongoose.EntityManagerImpl;
-import com.darekzon.shoppinglist.repository.ProductRepository;
-import com.darekzon.shoppinglist.repository.ProductRepositoryImpl;
+import com.darekzon.shoppinglist.mongoose.Sendmail;
+import com.darekzon.shoppinglist.mongoose.SendmailImpl;
+import com.darekzon.shoppinglist.repository.ProductListRepository;
+import com.darekzon.shoppinglist.repository.ProductListRepositoryImpl;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 
@@ -12,7 +14,8 @@ public class RepositoryModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(EntityManager.class).to(EntityManagerImpl.class).in(Scopes.SINGLETON);
-		bind(ProductRepository.class).to(ProductRepositoryImpl.class);
+		bind(Sendmail.class).to(SendmailImpl.class).in(Scopes.SINGLETON);
+		bind(ProductListRepository.class).to(ProductListRepositoryImpl.class);
 	}
 
 }
