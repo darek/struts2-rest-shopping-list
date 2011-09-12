093566a84f24ffd94e75e669f3942e120df98b9b
Darek Zon <darek@alchemist.(none)>
2011-09-12 23:09:27 +0200

    added new functions to javascript

--- web/js/main.js
+++ web/js/main.js
@@ -1,6 +1,6 @@
 /** main.js **/
 
-function initAddProductForm(){
+function initAddProductForm(id){
 	var form = $("form#add-product-form");
 	var submit = form.find("#add-product-form_0");
 	var name = form.find("#add-product-form_product_name");
@@ -10,6 +10,7 @@ function initAddProductForm(){
 	   submit.addClass("disable");
 	   submit.attr("disabled","disabled");
 		$.ajax({
+			url: ContextPath+"/product/"+id,
 	 		type: "post",
 	 		data: {"product.name":name.val(),"product.amount":amount.val()}
 	 	})
@@ -31,4 +32,32 @@ function initAddProductForm(){
 		
 	});
 
+}
+
+function bindDeleteProductsLinks(id){
+ $("#products-list li a").click(function(){
+ 	var pid = $(this).attr("id");
+ 	$.ajax({
+ 		url: ContextPath+"/product/"+id+"?productId="+pid,
+ 		type: "delete"
+ 	})
+ 	.success(function(){
+ 		console.log("suc");
+ 	})
+ 	.error(function(){
+ 		console.log("error");
+ 	})
+ 	.complete(function(){
+ 		console.log("compl");
+ 	});
+ 		
+ 		
+ 		
+ 	return false;
+ 	
+ });	
+}
+
+function bindDeleteProductsListLink(id){
+	
 }
