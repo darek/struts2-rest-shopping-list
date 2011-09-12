f6c22c7cdb5dd01e4c424685a6613391d4a98999
Darek Zon <darek@alchemist.(none)>
2011-09-12 23:05:46 +0200

    moved converters to new package

---
+++ src/com/darekzon/shoppinglist/converter/ObjectIdConverter.java
@@ -0,0 +1,30 @@
+package com.darekzon.shoppinglist.converter;
+
+import java.util.Map;
+
+import org.apache.struts2.util.StrutsTypeConverter;
+import org.bson.types.ObjectId;
+
+import com.opensymphony.xwork2.conversion.TypeConversionException;
+
+public class ObjectIdConverter extends StrutsTypeConverter {
+
+	@Override
+	public Object convertFromString(Map arg0, String[] arg1, Class arg2) {
+		if(arg1.length>0){
+			return new ObjectId(arg1[0]);
+		}
+		throw new TypeConversionException();
+	}
+
+	@Override
+	public String convertToString(Map arg0, Object arg1) {
+		return arg1.toString();
+	}
+	
+	@Override
+    protected Object performFallbackConversion(Map context, Object o, Class toClass) {
+			return new Object();
+	}
+
+}
